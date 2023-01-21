3751e5b8b78884bedf90edbb9b4b206989586a7a
Yehor <106986179+YehorBelenkov@users.noreply.github.com>
2023-01-21 13:18:45 -0600

    Created a MEVIOContext

---
+++ MEVIO/Models/MEVIOContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace MEVIO.Models
+{
+    public class MEVIOContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; } = null!;
+        public MEVIOContext(DbContextOptions<MEVIOContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ MEVIO/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace MEVIO.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string StudentName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string StudyEmail { get; set; }
+        public string StudentCode { get; set; }
+        public string? PassportNumber { get; set; }
+        public string? DateOfIssuePassport { get; set; }
+        public string? TIN { get; set; }
+        public bool Discount { get; set; }
+        public string? Discount_Description { get; set; }
+    }
+}
