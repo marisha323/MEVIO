9fb15edc4e9c99dde0d2a0e96b333fe45fdd9bde
Yehor <106986179+YehorBelenkov@users.noreply.github.com>
2023-01-26 20:59:53 -0600

    Added controllers from the Uml Diagram

---
+++ MEVIO/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MEVIO.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
