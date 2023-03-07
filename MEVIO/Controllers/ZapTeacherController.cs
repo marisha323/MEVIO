675aadbc9498d5805be480938d57c8f8d23821f9
Shvrpness <120966385+Shvrpness@users.noreply.github.com>
2023-03-07 23:26:34 +0200

    Добавил папки

---
+++ MEVIO/Controllers/ZapTeacherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MEVIO.Controllers
+{
+    public class ZapTeacherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
