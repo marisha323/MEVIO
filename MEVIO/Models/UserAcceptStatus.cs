d729d522200638dbca74c413302b8567a5794893
Turner Max <voloshok.maksi@ukr.net>
2023-03-13 22:14:05 +0200

    MainFull(99%)

--- MEVIO/Models/UserAcceptStatus.cs
+++ MEVIO/Models/UserAcceptStatus.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public int? UserId { get; set; }
         public virtual User User { get; set; }
+        public int? MeasureId { get; set; }
+        public virtual Measure Measure { get; set; }
         public bool IsAccept { get; set; } //принял не принял преглашения
 
         
