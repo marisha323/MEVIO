bd5ad9c966357658ccee2cc9e1310950925eb5d6
Turner Max <voloshok.maksi@ukr.net>
2023-04-03 18:34:59 +0300

    1

--- MEVIO/Controllers/TableUsersAllController.cs
+++ MEVIO/Controllers/TableUsersAllController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DocumentFormat.OpenXml.InkML;
+using MEVIO.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MEVIO.Controllers
 {
     public class TableUsersAllController : Controller
     {
+        public MEVIOContext context;
+
+        public TableUsersAllController(MEVIOContext context)
+        {
+            this.context = context;
+        }
         public IActionResult Index()
         {
+            ViewBag.Users = context.Users.ToList();
+            
             return View();
         }
     }
