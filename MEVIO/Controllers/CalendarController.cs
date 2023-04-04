83cd624a8ea721cca9c398b9a8f5676526ea5b32
Yehor <106986179+YehorBelenkov@users.noreply.github.com>
2023-04-03 23:14:12 -0500

    saving calendar controller changes

--- MEVIO/Controllers/CalendarController.cs
+++ MEVIO/Controllers/CalendarController.cs
@@ -64,7 +64,7 @@ namespace MEVIO.Controllers
         public async Task<IActionResult> EditMeasure()
         {
             var addedclients = Request.Form["AddedClients"];
-            var selectedVenue = Request.Form["selectedVenue"];
+             m,var selectedVenue = Request.Form["selectedVenue"];
             var monthbegin = Request.Form["MeasureBeginI"];
             var monthend = Request.Form["MeasureEndI"];
             var measureName = Request.Form["MeasureNameI"];
