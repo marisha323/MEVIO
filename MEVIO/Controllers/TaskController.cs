369b49d851eb7e416da0555b02f3d26147439d4a
Iryna1122 <Oliinykirina1122@gmail.com>
2023-04-07 20:39:16 +0300

    task

--- MEVIO/Controllers/TaskController.cs
+++ MEVIO/Controllers/TaskController.cs
@@ -1,6 +1,7 @@
 ﻿using MEVIO.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Net.Sockets;
+using System.Text.Json;
 
 namespace MEVIO.Controllers
 {
@@ -8,33 +9,37 @@ namespace MEVIO.Controllers
     {
         MEVIOContext context;
 
+        MEVIO.Models.User user = null;
+
         public TaskController(MEVIOContext context)
         {
             this.context = context;
         }
+
         public IActionResult Index()
         {
-            var listUserId = context.TasksWatchingPersons.Select(o => o.UserId).ToList();
+          
+            ViewBag.Users=context.Users.ToList();
 
-            var watchingPersonUserIds = context.TasksWatchingPersons
-           .Where(twp => twp.UserId != null)
-           .Select(twp => twp.UserId.Value)
-           .Distinct();
+           // var watchingPersonUserIds = context.TasksWatchingPersons
+           //.Where(twp => twp.UserId != null)
+           //.Select(twp => twp.UserId.Value)
+           //.Distinct();
 
-            var watchingPersons = context.Users
-             .Where(u => watchingPersonUserIds.Contains(u.Id))
-             .ToList();
+           // var watchingPersons = context.Users
+           //  .Where(u => watchingPersonUserIds.Contains(u.Id))
+           //  .ToList();
 
-            ViewBag.WatchingPersons = watchingPersons;
+           // ViewBag.WatchingPersons = watchingPersons;
 
 
-            var responsiblePersonUserId = context.TaskResponsiblePersons.Where(rp => rp.UserId != null)
-                .Select(rp => rp.UserId.Value)
-                .Distinct();
+           // var responsiblePersonUserId = context.TaskResponsiblePersons.Where(rp => rp.UserId != null)
+           //     .Select(rp => rp.UserId.Value)
+           //     .Distinct();
 
-            var responsiblePersons = context.Users.Where(u => responsiblePersonUserId.Contains(u.Id)).ToList();
+           // var responsiblePersons = context.Users.Where(u => responsiblePersonUserId.Contains(u.Id)).ToList();
 
-            ViewBag.Resp = responsiblePersons;
+           // ViewBag.Resp = responsiblePersons;
 
             return View();
         }
@@ -44,19 +49,76 @@ namespace MEVIO.Controllers
         [HttpPost]
         public async Task<ActionResult> AddTask(string TaskName, string Description)
         {
-           
+            //Get Id of create User
+            string UserLoggedIn = HttpContext.Request.Cookies["UserLoggedIn"];
 
-            //Sasha
+            user = JsonSerializer.Deserialize<MEVIO.Models.User>(UserLoggedIn);
 
-            //context.Tasks.Add(new()
-            //{
-            //    Description = Description,
-            //    TaskName = TaskName
-            //});
+            int userId = user.Id;
+            string creator = user.UserName;
 
-            //await context.SaveChangesAsync();
+            //Get date & time
+            string dateField1 = Request.Form["dateField1"];
+            string timeField1 = Request.Form["timeField1"];
 
-            return View();
+            DateTime dateTime1 = DateTime.Parse(dateField1 + " " + timeField1);
+
+            string dateField2 = Request.Form["dateField2"];
+            string timeField2 = Request.Form["timeField2"];
+
+            DateTime dateTime2 = DateTime.Parse(dateField2 + " " + timeField2);
+
+
+            //All Watching persons
+            var idsWatchPersons = Request.Form["watchIdd"];
+
+            //All Perponsible persons
+            var idsResponsiblePersons = Request.Form["respIdd"];
+
+            string underTaskName = Request.Form["underTask"];
+
+            MEVIO.Models.Task task = new Models.Task
+            {
+
+                TaskName = TaskName,
+                Description = Description,
+                IsImportant= true,
+                Begin= dateTime1,
+                End= dateTime2
+
+            };
+
+            if (task != null)
+            {
+                context.Tasks.Add(task);
+                context.SaveChanges();
+            }
+            // Last ID of Task
+            var lastIdTask = context.Tasks.ToList().LastOrDefault().Id;
+
+
+
+            foreach (var clientsId in idsWatchPersons)
+            {
+                var idsWatchper = new TaskResponsiblePersons
+                {
+                    TaskId = lastIdTask,
+                    UserId=userId,
+
+                    
+                    // IsCreator = false //устанавливаем false, поскольку это не создатель события
+                };
+                context.TaskResponsiblePersons.Add(idsWatchper); //добавляем экземпляр EventsUsers в контекст базы данных
+            }
+
+            context.SaveChanges(); //сохраняем изменения в базе данных
+
+
+
+
+
+            return Redirect("/Home/Index");
+        
         }
     }
 }
